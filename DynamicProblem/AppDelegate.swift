ed82cd1a6376d0155ea520cb29e7c955c5c99227
Vojtech Rinik <vojto@rinik.net>
2017-04-06 13:53:53 +0200

    Initial Commit

---
+++ DynamicProblem/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DynamicProblem
+//
+//  Created by Vojtech Rinik on 4/6/17.
+//  Copyright © 2017 Vojtech Rinik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
